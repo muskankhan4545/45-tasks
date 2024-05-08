61eea9acf3d4ee11c9d6742f0ffff9d9f2b04679
Muskansirajkhan <164318683+muskankhan4545@users.noreply.github.com>
2024-05-08 08:48:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Excercise #2
+//personal messages:
+/*
+store a person's name in a variable,and print a message to that person .your
+message should be a simple,such as "Hello Eric",would you like to learn some
+python today?
+*/
+var personname = "ilahibux";
+console.log("Hello" + personname + "would you like to learn some python today?");
---
+++ main.ts
@@ -0,0 +1,11 @@
+//Excercise #2
+//personal messages:
+
+/*
+store a person's name in a variable,and print a message to that person .your 
+message should be a simple,such as "Hello Eric",would you like to learn some 
+python today?
+*/
+
+let personname:string ="ilahibux";
+console.log("Hello" + personname + "would you like to learn some python today?")
